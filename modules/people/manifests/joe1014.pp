b02c7fef498eb25f43e01a40e8e59340ea2c6b4b
Masato Noguchi <masatonoguchi@Masato-no-MacBook-Air.local>
2014-02-12 04:36:57 +0900

    add file

---
+++ modules/people/manifests/joe1014.pp
@@ -0,0 +1,33 @@
+class people::joe1014 {
+  # 自分の環境で欲しいresourceをincludeする
+  include iterm2::stable
+
+  $home     = "/Users/${::luser}"
+  $src      = "${home}/src"
+  $dotfiles = "${src}/dotfiles"
+
+  # ~/src/dotfilesにGitHub上のjoe1014/dotfilesリポジトリを
+  # git-cloneする。そのとき~/srcディレクトリがなければいけない。
+  repository { $dotfiles:
+    source  => "joe1014/dotfiles",
+    require => File[$src];
+
+    "${home}/.vim/bundle/neobundle.vim":
+    source  => "Shougo/neobundle.vim";
+  }
+  # git-cloneしたらインストールする
+  exec { "sh ${dotfiles}/install.sh":
+    cwd => $dotfiles,
+    creates => "${home}/.zshrc",
+    require => Repository[$dotfiles],
+  }
+  package {
+    'Kobito':
+      source   => "http://kobito.qiita.com/download/Kobito_v1.8.7.zip",
+      provider => compressed_app;
+    'KeyRemap4MacBook':
+      source   => "https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-9.2.0.dmg",
+      provider => pkgdmg;
+  }
+
+}
